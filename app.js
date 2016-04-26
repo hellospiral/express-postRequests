248af01e954a7cfe69e4cb688ff32b1d03f169ed
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-26 21:04:00 +0000

    add body parser, get post request adding new friend to array and redirecting to /friends

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+var friends = ["Mike", "Billy", "Joey", "Lilly", "Rachael"];
+
 
 app.get("/", function(req, res) {
     res.render("home");
 });
 
 app.get("/friends", function(req, res) {
-    var friends = ["Mike", "Billy", "Joey", "Lilly", "Rachael"];
     res.render("friends", {friends: friends});
 });
 
 app.post("/addFriend", function(req, res) {
-    res.send("YOU HAVE REACHED THE POST ROUTE");
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
